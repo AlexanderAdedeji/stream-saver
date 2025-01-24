d82ce4b0f8ebd586ba4bc7f7d332176ece808a43
AlexAdedeji <alexadedeji15@gmail.com>
2025-01-24 11:57:31 -0800

    created tanstack provider

---
+++ config/nav.tsx
@@ -0,0 +1,41 @@
+export interface NavProps {
+  href: string;
+  label: string;
+}
+
+// export const navbarLinksList: NavProps[] = [
+//   {
+//     href: "#features",
+//     label: "Features",
+//   },
+//   {
+//     href: "#testimonials",
+//     label: "Testimonials",
+//   },
+//   {
+//     href: "#pricing",
+//     label: "Pricing",
+//   },
+//   {
+//     href: "#faq",
+//     label: "FAQ",
+//   },
+// ];
+export const navbarLinksList: NavProps[] = [
+  {
+    href: "/youtube",
+    label: "Youtube",
+  },
+  {
+    href: "/instagram",
+    label: "Instagram",
+  },
+  {
+    href: "/#pricing",
+    label: "Pricing",
+  },
+  // {
+  //   href: "#faq",
+  //   label: "FAQ",
+  // },
+];
---
+++ config/site.tsx
@@ -0,0 +1,28 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "StreamSave",
+  title: "Stream Save",
+  description:
+    "Stream Save is a startup that allows you to use to download videos from youtube, facebook, instagram and whatsapp",
+  mainNav: [
+    {
+      title: "Pricing",
+      href: "/pricing",
+    },
+    {
+      title: "Newsletter",
+      href: "/newsletter",
+    },
+    {
+      title: "Explore",
+      href: "/browse",
+    },
+  ],
+  links: {
+    twitter: "https://twitter.com/YetuAiNA",
+    github: "https://github.com/YetuAi-Demos",
+    docs: "https://YetuAi.io/docs",
+    api: "https://api.YetuAi.io",
+  },
+};
