268ab47684ff21c4a289be4644292a6bba7e8fe4
AlexAdedeji <alexadedeji15@gmail.com>
2025-01-31 01:51:26 -0800

    added visit tracking

---
+++ adapters/analytics/index.ts
@@ -0,0 +1,20 @@
+import { ApiNoAuth } from "..";
+
+// Function to track visits
+const trackVisits = async () => {
+  try {
+    const response = await ApiNoAuth.post(`/analytics/track-visit`, {
+      headers: {
+        "Content-Type": "application/json",
+        "User-Agent": navigator.userAgent || "Unknown",
+        Referer: document.referrer || "Direct Visit",
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error logging visit:", error);
+    throw new Error("Failed to log visit");
+  }
+};
+
+export const analyticsService = { trackVisits };
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { analyticsService } from "@/adapters/analytics";
 import { About } from "@/components/general/About";
 import { Cta } from "@/components/general/Cta";
 import { FAQ } from "@/components/general/FAQ";
@@ -7,13 +10,21 @@ import { HowItWorks } from "@/components/general/HowItWorks";
 import { Newsletter } from "@/components/general/Newsletter";
 import { Pricing } from "@/components/general/Pricing";
 import { Services } from "@/components/general/Services";
+import { useMutation } from "@tanstack/react-query";
+import { useEffect } from "react";
 
 export default function Home() {
+  const { mutate } = useMutation({
+    mutationFn: analyticsService.trackVisits,
+  });
+  useEffect(() => {
+    mutate();
+  }, []);
   return (
     <>
-     <Hero />
+      <Hero />
       <About />
-       <HowItWorks />
+      <HowItWorks />
       <Features />
       <Services />
       <Cta />
